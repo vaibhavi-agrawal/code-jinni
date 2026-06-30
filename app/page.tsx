222548d9be3fdfd0168af5034054d11a084facfa
vaibhavi-agrawal <vaibhaviagrawal05@gmail.com>
2026-06-30 22:17:00 +0530

    Setup Prisma and MongoDB

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
+
   return (
     <div className = "flex flex-col items-center justify-center min-h-screen py-2">
       <Button>Click me</Button>
