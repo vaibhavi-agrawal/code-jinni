3d8208134414fb932ee19bdc185fd37980f6968a
vaibhavi-agrawal <vaibhaviagrawal05@gmail.com>
2026-06-30 21:25:37 +0530

    Initial structure of next.js and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className = "flex flex-col items-center justify-center min-h-screen py-2">
+      <Button>Click me</Button>
+    </div>
+  );
+}
